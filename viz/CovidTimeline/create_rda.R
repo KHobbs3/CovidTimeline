47578e67d187d72abedd336264791ea71ca8dbaf
kt <kthobbs@mail.ubc.ca>
2021-02-15 07:10:46 -0800

    Updated February 15, 2021.

--- viz/CovidTimeline/create_rda.R
+++ viz/CovidTimeline/create_rda.R
@@ -10,21 +10,19 @@
 
 library(here) # file paths
 library(rgdal) # reading shapefiles
+library(ggplot2) # graphing
+library(plotly) # interactivity
 
-# READ ------------------------------------------
 # read covid data
 master <- rgdal::readOGR(
-  dsn= paste0(here(),sprintf("/data/input/cases_%s.shp",format(Sys.time(), "%Y-%m-%d"))), 
-  layer=sprintf("cases_%s",format(Sys.time(), "%Y-%m-%d")),
+  dsn= paste0(here(),sprintf("/data/input/cases_%s.shp",Sys.Date())), 
+  layer=sprintf("cases_%s", Sys.Date()),
   verbose=FALSE
 )
 
 # read intervention data
-interv <- read.csv(paste0(here(), sprintf("/data/input/InterventionScan_Processed_%s.csv",format(Sys.time(), "%Y-%m-%d"))))
+interv <- read.csv(paste0(here(), sprintf("/data/input/InterventionScan_Processed_%s.csv",Sys.Date())))
 
-
-
-# CLEAN -----------------------------------------
 # convert covid date type
 master$Date <- as.Date(as.character(master$date_repor), "%Y-%m-%d")
 
@@ -36,16 +34,12 @@ master.df$cumulative <- as.numeric(as.character(master.df$cumulative))
 master.df$cases <- as.numeric(as.character(master.df$cases))
 
 interv$Date <- as.Date(as.character(interv$Date.implemented),"%Y-%m-%d")
-interv$suggested_industry <- as.character(interv$suggested_industry )
+
 
 # clean column names
 colnames(interv)[7] = "Intervention"
 
-
-
-# SAVE ------------------------------------------
 # save as RData
 save(master.df, file = here("/data/output", "covid.rda"))
 save(interv, file = here("/data/output/", "interventions.rda"))
-
 
