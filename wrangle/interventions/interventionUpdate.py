0c327f5a2ee65c75e48dc796930f0d32fc2b67fc
kt <kthobbs@mail.ubc.ca>
2021-02-22 13:08:04 -0800

    Updated February 22, 2021.

--- wrangle/interventions/interventionUpdate.py
+++ wrangle/interventions/interventionUpdate.py
@@ -1,36 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-### Processing Interventions
-
+# ### Processing Interventions
+# 
+# * **Step 1:** Create dictionary of key words (see industries-dict tab in place-types-concordance.xlsx for keywords used to identify industry).
+# * **Step 2:** Add `Industry` tags.
+# * **Step 3:** Filter interventions so that only "Closures, Openings, Restrictions, and Restriction releases" are retained.
+# 
+# Tags were validated by comparing manually labelled entries with suggested tags. Mismatches were either attributed to incorrect/redundant/inapplicable keywords or manual tags.
 
 import pandas as pd
-from collections import defaultdict
 import datetime as dt
 
+
 # Step 1 ---
-import os
-os.chdir("CovidTimeline/wrangle/interventions/")
+d = dict()
+d = pd.read_excel('data/interventions/place-types-concordance.xlsx', sheet_name = 'industries-dict').set_index('industry').transpose().to_dict('records', into=d)
 
-d = defaultdict(list)
-d = pd.read_excel('../data/interventions/place-types-concordance.xlsx', sheet_name = 'industries-dict').set_index('industry').transpose().to_dict('records', into=d)
-
-interv = pd.read_csv('../data/interventions/InterventionScan_Nov.csv')
+interv = pd.read_excel('data/interventions/master_closures_openings.xlsx', sheet_name = "top30")
 
 
-
-for item in d:
-    for key,value in item.items():
-        print(key, value)
-
-
-print("Printing key-terms dictionary................")
-d2 = {key: value.split(', ') for key, value in item.items() for item in d} 
-
+d2 = {key: value.split(', ') for key, value in d[0].items()} 
 
 
 # Step 2 ---
-print("Labelling intervention data................")
 def retrieve_industry(x, dictionary):
     """
     x is a summary description of the policy intervention formatted as a string
@@ -42,6 +35,7 @@ def retrieve_industry(x, dictionary):
     try:
         for k in dictionary.keys():
             for v in dictionary[k]:
+    #             print('checking for {}'.format(v))
                 if x.lower().find(v) > -1:
                     tags.add(k)
     #                 print('found {} in summary, adding {}'.format(v, k))
@@ -60,16 +54,12 @@ def retrieve_industry(x, dictionary):
 interv['suggested_industry'] = interv['Intervention summary'].map(lambda x: retrieve_industry(x, d2))
 
 
-
 # Step 3 --- 
-print("Only retaining Openings, Closures, Restrictions................")
 interv = interv.loc[interv['Intervention category'].str.contains("Openings|Closures|Restrictions|Restriction release") == True]
 
 
-interv.drop(columns = 'Unnamed: 0', inplace = True)
+
 latest = dt.datetime.today().strftime("%Y-%m-%d")
 print("Creating file: InterventionScan_Processed_{}.csv".format(latest))
-
-interv.to_csv('../../viz/CovidTimeline/data/input/InterventionScan_Processed_{}.csv'.format(latest), encoding = 'utf-8-sig')
-print("Done.")
+interv.to_csv('../viz/CovidTimeline/data/input/InterventionScan_Processed_{}.csv'.format(latest), encoding = 'utf-8-sig')
 
